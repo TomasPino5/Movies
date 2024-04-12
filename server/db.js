1984bec65bf6f7ec1797ff9ce96c301675b88148
TomasPino5 <tomaspino48@gmail.com>
2024-04-12 19:52:53 -0300

    db

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 let connectionDb
 module.exports = {
     connectToDb: (cb) => {
-        MongoClient.connect(`mongodb+srv://tomaspino48:velestig5@cluster0.43bl2vl.mongodb.net/moviestore?retryWrites=true&w=majority&appName=Cluster0`)
+        MongoClient.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.43bl2vl.mongodb.net/moviestore?retryWrites=true&w=majority&appName=Cluster0`)
         .then((client) => {
             connectionDb = client.db()
             return cb()
